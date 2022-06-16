124a8269a08cbf00cb2a9e4968c462b3bcab92fb
nikitkhakholia <khakholia.nk@gmail.com>
2022-06-16 13:21:38 +0530

    Updates

---
+++ precache-manifest.deac3dd85935dae2bb06433c39c1cb20.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "2af06370e2b836eb4977",
+    "url": "/vc/static/css/main.5fbaf775.chunk.css"
+  },
+  {
+    "revision": "2af06370e2b836eb4977",
+    "url": "/vc/static/js/main.2af06370.chunk.js"
+  },
+  {
+    "revision": "dc488f304914c16accdd",
+    "url": "/vc/static/css/1.b7973cc5.chunk.css"
+  },
+  {
+    "revision": "dc488f304914c16accdd",
+    "url": "/vc/static/js/1.dc488f30.chunk.js"
+  },
+  {
+    "revision": "bf7e5f5fe57b98b4f15d",
+    "url": "/vc/static/js/runtime~main.bf7e5f5f.js"
+  },
+  {
+    "revision": "e02f123bafd7b243b8972edc5abb4ee1",
+    "url": "/vc/static/media/promotions.e02f123b.jpeg"
+  },
+  {
+    "revision": "059a90d6acd701deab0edee66e165f33",
+    "url": "/vc/static/media/check.059a90d6.svg"
+  },
+  {
+    "revision": "609d5f36fb9981a290afa0ae8208c37f",
+    "url": "/vc/static/media/pointing-goal.609d5f36.png"
+  },
+  {
+    "revision": "b8ae2ff0c5ec37e14cd9063084275c08",
+    "url": "/vc/static/media/blue-bg.b8ae2ff0.jpeg"
+  },
+  {
+    "revision": "0e8896d984f6c89e600d6bc85e5f7b51",
+    "url": "/vc/static/media/addmissions.0e8896d9.jpg"
+  },
+  {
+    "revision": "6882d738d72ea6d731117e316811d49d",
+    "url": "/vc/static/media/profesional.6882d738.jpg"
+  },
+  {
+    "revision": "bf43adb2866f95d4c41bd28d12f48032",
+    "url": "/vc/static/media/steps.bf43adb2.png"
+  },
+  {
+    "revision": "430ca2b56a92c3a4882cf626994ba4bb",
+    "url": "/vc/index.html"
+  }
+];
